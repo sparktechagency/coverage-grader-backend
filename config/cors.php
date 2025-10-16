b719990dc82a37c324bb5a59de9dbcec3324d6e1
Kazi Omar Faruk <kaziomar144@gmail.com>
2025-10-16 16:17:20 +0600

    docker file change

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://66.29.135.26.nip.io'],
+    'allowed_origins' => ['http://66.29.135.26.nip.io', 'http://localhost:3000'],
 
     'allowed_origins_patterns' => [],
 
