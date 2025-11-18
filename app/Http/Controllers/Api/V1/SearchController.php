8c296af6e4ca112453308f40ec3da584ab29da69
Kazi Omar Faruk <kaziomar144@gmail.com>
2025-11-18 12:56:40 +0600

    search function update

--- app/Http/Controllers/Api/V1/SearchController.php
+++ app/Http/Controllers/Api/V1/SearchController.php
@@ -11,15 +11,43 @@ class SearchController extends Controller
 {
     public function search(Request $request)
     {
-        $query = $request->input('search');
         $perPage = $request->input('per_page', 10);
 
-        $provider = InsuranceProvider::where('name', 'LIKE', "%{$query}%")
-            ->orWhere('slug', 'LIKE', "%{$query}%")
-            ->paginate($perPage);
-        if($provider->isEmpty()){
+
+        $providersQuery = InsuranceProvider::query();
+
+
+        if ($request->filled('search')) {
+            $query = $request->input('search');
+
+
+            $providersQuery->where(function ($q) use ($query) {
+
+
+                $q->where('name', 'LIKE', "%{$query}%")
+                    ->orWhere('slug', 'LIKE', "%{$query}%");
+
+
+                $q->orWhereHas('states', function ($stateQuery) use ($query) {
+                    $stateQuery->where('name', 'LIKE', "%{$query}%")
+                        ->orWhere('code', 'LIKE', "%{$query}%");
+                });
+
+
+                $q->orWhereHas('policyCategories', function ($categoryQuery) use ($query) {
+                    $categoryQuery->where('name', 'LIKE', "%{$query}%")
+                        ->orWhere('slug', 'LIKE', "%{$query}%");
+                });
+            });
+        }
+
+
+        $provider = $providersQuery->paginate($perPage);
+
+        if ($provider->isEmpty()) {
             return response_error('No insurance providers found', [], 404);
         }
+
         return InsuranceProviderResource::collection($provider);
     }
 }
